6ec3ac8a2bfdfcae7d548bec1b172a6fabcb35da
AdvaySingh1 <advay@umich.edu>
2026-01-20 11:05:45 -0800

    Refactored Abc_NtkDupObj

--- src/base/abc/abcObj.c
+++ src/base/abc/abcObj.c
@@ -343,7 +343,7 @@ void Abc_NtkDeleteAll_rec( Abc_Obj_t * pObj )
 Abc_Obj_t * Abc_NtkDupObj( Abc_Ntk_t * pNtkNew, Abc_Obj_t * pObj, int fCopyName )
 {
     Abc_Obj_t * pObjNew;
-    char * pName;
+    char * pName = NULL;
     Abc_Frame_t * pAbc;
     pAbc = Abc_FrameGetGlobalFrame();
     // create the new object
@@ -357,19 +357,12 @@ Abc_Obj_t * Abc_NtkDupObj( Abc_Ntk_t * pNtkNew, Abc_Obj_t * pObj, int fCopyName 
             {
                 pName = Abc_ObjName(Abc_ObjFanout0Ntk(pObj));
                 Abc_ObjAssignName( pObjNew, pName, NULL );
-                // map to node retention manager
-                if ( pName )
-                {
-                    if ( pAbc && pAbc->pNodeRetention )
-                        Nr_ManAddOrigin( pAbc->pNodeRetention, pObjNew->Id, pName );
-                }
             }
         }
         else if ( Abc_ObjIsCo(pObj) )
         {
             if ( !Abc_NtkIsNetlist(pNtkNew) )
             {
-                char * pName;
                 if ( Abc_ObjIsPo(pObj) )
                     pName = Abc_ObjName(Abc_ObjFanin0Ntk(pObj));
                 else
@@ -378,12 +371,6 @@ Abc_Obj_t * Abc_NtkDupObj( Abc_Ntk_t * pNtkNew, Abc_Obj_t * pObj, int fCopyName 
                     pName = Abc_ObjName(pObj);
                 }
                 Abc_ObjAssignName( pObjNew, pName, NULL );
-                // map to node retention manager
-                if ( pName )
-                {
-                    if ( pAbc && pAbc->pNodeRetention )
-                        Nr_ManAddOrigin( pAbc->pNodeRetention, pObjNew->Id, pName );
-                }
             }
         }
         else if ( Abc_ObjIsBox(pObj) || Abc_ObjIsNet(pObj) )
@@ -416,9 +403,12 @@ Abc_Obj_t * Abc_NtkDupObj( Abc_Ntk_t * pNtkNew, Abc_Obj_t * pObj, int fCopyName 
         pObjNew->pData = pObj->pData;
     pObjNew->fPersist = pObj->fPersist;
     // transfer HAIG
-//    pObjNew->pEquiv = pObj->pEquiv;
     // remember the new node in the old node
     pObj->pCopy = pObjNew;
+//    pObjNew->pEquiv = pObj->pEquiv;
+    // map to node retention manager (once, at the end)
+    if ( pName && pAbc && pAbc->pNodeRetention )
+        Nr_ManAddOrigin( pAbc->pNodeRetention, pObjNew->Id, pName );
     return pObjNew;
 }
 

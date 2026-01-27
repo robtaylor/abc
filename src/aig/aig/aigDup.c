54ce3f8ce9d0f9ffa200332758f5c2dba310a09b
AdvaySingh1 <advay@umich.edu>
2026-01-27 13:25:40 -0800

    fixed Aig_ManDupDfsGuided_rec obj num

--- src/aig/aig/aigDup.c
+++ src/aig/aig/aigDup.c
@@ -689,9 +689,9 @@ Aig_Obj_t * Aig_ManDupDfsGuided_rec( Aig_Man_t * pNew, Aig_Man_t * p, Aig_Obj_t 
     }
     if ( !Aig_ManDupDfsGuided_rec( pNew, p, Aig_ObjFanin1(pObj) ) )
         return NULL;
-    nNodesBefore = Aig_ManNodeNum(pNew);
+    nNodesBefore = Aig_ManObjNum(pNew);
     pObjNew = Aig_Oper( pNew, Aig_ObjChild0Copy(pObj), Aig_ObjChild1Copy(pObj), Aig_ObjType(pObj) );
-    nNodesAfter = Aig_ManNodeNum(pNew);
+    nNodesAfter = Aig_ManObjNum(pNew);
     if ( pEquivNew )
     {
         if ( pNew->pEquivs )
